820068691e7115b197e02cc779b033f983800543
rsuan-lab <47155949+richardsuan@users.noreply.github.com>
2023-05-31 11:28:02 -0500

    Update EmployeeService.java

--- src/main/java/com/prueba/thales/impl/EmployeeService.java
+++ src/main/java/com/prueba/thales/impl/EmployeeService.java
@@ -1,64 +1,43 @@
 package com.prueba.thales.impl;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.prueba.thales.dto.Data;
 import com.prueba.thales.dto.ResponseEmployeeDto;
 import com.prueba.thales.utils.Controller;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.http.*;
 import org.springframework.web.client.RestTemplate;
 import com.prueba.thales.dto.EmployeeDto;
 
 
-import java.net.URI;
 import java.util.ArrayList;
-import java.util.Objects;
-
+import java.util.List;
 public class EmployeeService {
     private static final Logger logger = LoggerFactory.getLogger(Controller.class);
     private  final String url ="https://dummy.restapiexample.com/api/v1";
 
-    public ArrayList<EmployeeDto> getHttpResponseOneEmployee(int id){
+    public List<EmployeeDto> getHttpResponseOneEmployee(int id) {
         // String url = String.format("%s/employee/%s", this.url, id);
         String url = "https://run.mocky.io/v3/40e14ba1-ee48-4dc3-8d05-c0e5e2799cc6";
         RestTemplate restTemplate = new RestTemplate();
-        HttpHeaders headers = new HttpHeaders();
-        headers.set("Connection", "keep-alive");
-        HttpEntity entity = new HttpEntity<>(headers);
-        logger.info("los datos son: {} {}", url, id);
-        ArrayList<EmployeeDto> employeeList = new ArrayList<EmployeeDto>();
-        try {
-            ResponseEntity<ResponseEmployeeDto> responseEntity = restTemplate.exchange(url, HttpMethod.GET,entity, ResponseEmployeeDto.class);
-            EmployeeDto employee = new EmployeeDto(Objects.requireNonNull(responseEntity.getBody()).getData());
-            if (employeeList.add(employee)){
-                return employeeList;
-            }
-
-
-
-        }catch (Exception e){
-            logger.info("error {}",e.getMessage());
-        }
-
-        return null;
+        return getEmployeeDtos(url, restTemplate);
     }
-    public String getHttpResponseEmployees(){
+
+    public List<EmployeeDto> getHttpResponseEmployees(){
         // String url = String.format("%s/employees/", this.url);
         String url = "https://run.mocky.io/v3/152ccd2f-0e83-4d11-b65a-a90be4d651f2";
         RestTemplate restTemplate = new RestTemplate();
-        HttpHeaders headers = new HttpHeaders();
-        headers.set("Connection", "keep-alive");
-        HttpEntity entity = new HttpEntity<>(headers);
-        logger.info("los datos son: {}", url);
+        return getEmployeeDtos(url, restTemplate);
+    }
+    private List<EmployeeDto> getEmployeeDtos(String url, RestTemplate restTemplate) {
+        List<EmployeeDto> employeeList = new ArrayList<>();
         try {
-            ResponseEntity<String> responseEntity = restTemplate.getForEntity(url, String.class);
-            logger.info("response {} \n {}", responseEntity, responseEntity.getBody());
-            return responseEntity.getBody();
-        }catch (Exception e){
-            logger.info("error {}",e.getMessage());
+            ResponseEmployeeDto response = restTemplate.getForObject(url, ResponseEmployeeDto.class);
+            if (response != null && response.getStatus().equals("success")) {
+                List<EmployeeDto> dataList = response.getData();
+                employeeList.addAll(dataList);
+            }
+        } catch (Exception e) {
+            logger.info("error {}", e.getMessage());
         }
-
-        return null;
+        return employeeList;
     }
 }
