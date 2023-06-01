82d3760347d74a52d7a0a65938009a371df892f2
rsuan-lab <47155949+richardsuan@users.noreply.github.com>
2023-06-01 01:45:26 -0500

    Update EmployeeService.java

--- src/main/java/com/prueba/thales/impl/EmployeeService.java
+++ src/main/java/com/prueba/thales/impl/EmployeeService.java
@@ -7,21 +7,22 @@ import org.slf4j.LoggerFactory;
 import org.springframework.web.client.RestTemplate;
 import com.prueba.thales.dto.EmployeeDto;
 
-
 import java.util.ArrayList;
 import java.util.List;
 public class EmployeeService {
     private static final Logger logger = LoggerFactory.getLogger(Controller.class);
-    private  final String url ="https://dummy.restapiexample.com/api/v1";
-    private  final String urlEmployees = "https://run.mocky.io/v3/152ccd2f-0e83-4d11-b65a-a90be4d651f2";
-    private  final String urlEmployee = "https://run.mocky.io/v3/40e14ba1-ee48-4dc3-8d05-c0e5e2799cc6";
+    private  String url ="https://dummy.restapiexample.com/api/v1";
+    private  String urlEmployees = "https://run.mocky.io/v3/152ccd2f-0e83-4d11-b65a-a90be4d651f2";
+    private  String urlEmployee = "https://run.mocky.io/v3/40e14ba1-ee48-4dc3-8d05-c0e5e2799cc6";
 
     public List<EmployeeDto> getHttpResponseOneEmployee(int id) {
-        String url = String.format("%s/employee/%s", this.url, id);
+        String url = String.format("%s/employee/%s", this.urlEmployee, id);
         // String url = "https://run.mocky.io/v3/40e14ba1-ee48-4dc3-8d05-c0e5e2799cc6";
+        logger.info(String.format("el id a buscar es %s %s", id, url));
         RestTemplate restTemplate = new RestTemplate();
-        if (getEmployeeDtos(url, restTemplate).isEmpty()){
-            return getEmployeeDtos( urlEmployees, restTemplate);
+        List<EmployeeDto> objRes = getEmployeeDtos(urlEmployee, restTemplate);
+        if (!objRes.isEmpty()){
+            return getEmployeeDtos(urlEmployee, restTemplate);
         }
         return getHttpResponseEmployees();
     }
@@ -33,6 +34,7 @@ public class EmployeeService {
     }
     private List<EmployeeDto> getEmployeeDtos(String url, RestTemplate restTemplate) {
         List<EmployeeDto> employeeList = new ArrayList<>();
+        logger.info(String.format("getEmployeeDtos url %s", url));
         try {
             ResponseEmployeeDto response = restTemplate.getForObject(url, ResponseEmployeeDto.class);
             if (response != null && response.getStatus().equals("success")) {
